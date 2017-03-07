ea98324ecea5876b8c023f2726109af2c4b1d148
sihuafeng <sihuafeng@HF-Mac-mini.local>
2017-03-07 17:24:55 +0800

    Initial Commit

---
+++ HFAVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFAVPlayer
+//
+//  Created by 司华锋 on 2017/3/7.
+//  Copyright © 2017年 HF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
